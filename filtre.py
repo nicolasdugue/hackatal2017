aca2dc15ab7669571abe7e41894df9c34f95df2f
nicolasdugue <dugue.nic@gmail.com>
2017-06-25 00:35:03 +0200

    Filtre

--- filtre.py
+++ filtre.py
@@ -1,12 +1,11 @@
 import sys
+import numpy as np
 
 fichier=open("voc.tsv")
 dico=set()
 
 borneMin=int(sys.argv[1])
 borneMax=int(sys.argv[2])
-ratio=float(sys.argv[3])
-ratio2=float(sys.argv[4])
 cpt=0
 
 error=0
@@ -14,10 +13,11 @@ print borneMin, borneMax
 for ligne in fichier:
 	try:
 		tab=ligne.split("\t")
-		freq=int(tab[1].strip())
-		rtio= float(tab[2]) / 120000
-		rt=float(tab[2])/float(tab[1])
-		if freq > borneMin and freq < borneMax and rtio < ratio and rt > ratio2:
+		mot=tab[0]
+		freq=map(int, list(tab[1].strip("[").strip("]").replace(" ", "").split(",")))
+		nbAnnee=int(tab[2])
+		docTotal=np.sum(np.array(freq))
+		if not mot.isdigit() and nbAnnee > 1 and docTotal > borneMin and docTotal < borneMax:
 			dico.add(tab[0].strip())
 			cpt+=1
 	except IndexError:
@@ -25,7 +25,7 @@ for ligne in fichier:
 		pass
 print cpt
 print error
-fichier =open("voc_freqmin"+str(borneMin)+"_freqmax"+str(borneMax)+"_ratiomax"+str(ratio), "w")
+fichier =open("voc_freqmin"+str(borneMin)+"_freqmax"+str(borneMax), "w")
 for mot in dico:
 	fichier.write(mot+"\n")
 fichier.close()
